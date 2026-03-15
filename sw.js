302cb7f7064872d3fd5850892e18d91cfa4c85db
Ositai <super92108@gmail.com>
2026-03-15 10:01:38 +0800

    Implement caching for PWA installation files
    Add caching for essential PWA files during installation.

--- sw.js
+++ sw.js
@@ -1,6 +1,15 @@
 // 這是 PWA 安裝必備的 Service Worker
 // 即使不快取任何檔案，也必須存在才能觸發安裝提示
 self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('v1').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json'
+      ]);
+    })
+  );
   console.log('Service Worker installed');
 });
 
